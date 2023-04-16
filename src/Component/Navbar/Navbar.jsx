bf442b69476e6d044ba88a134fe0b4372db5b461
VishalBhuse <vbhuse1997@gmail.com>
2023-04-16 22:48:11 +0530

    multi step form

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -104,7 +104,7 @@ const DesktopNav = ({ handleGetLoanClick }) => {
     >
       <Link to={"/"}>
         <Text fontSize={"1.5rem"} fontWeight="700">
-          Loanbridge Pro
+          Loanbridge Pros
         </Text>
       </Link>
       <HStack
